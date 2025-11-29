293136d5f999cfe67bd366e31245908120b4ecc2
Faizan <marral7654321@gmail.com>
2025-11-29 19:21:07 +0500

    feat:res

--- git-scope/app/components/layout/Header.tsx
+++ git-scope/app/components/layout/Header.tsx
@@ -1,18 +1,46 @@
-import AppLogo from "../ui/icons/ AppLogo";
+"use client";
+import React, { useState } from "react";
 import SettingsIcon from "../ui/icons/SettingsIcon";
 import ProfileIcon from "../ui/icons/ProfileIcon";
 import SearchBar from "../ui/SearchBar";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+import AppLogo from "../ui/icons/ AppLogo";
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
-    <div className="border-b-2 border-gray-500 p-6 flex items-center justify-between">
-      <AppLogo />
-      <div className="hidden md:flex items-center justify-center gap-7 mr-3">
-        <SearchBar />
-        <SettingsIcon />
-        <ProfileIcon />
+    <>
+      {/* HEADER */}
+      <div className="border-b-2 border-gray-500 p-6 flex items-center justify-between">
+        <AppLogo />
+
+        {/* Desktop Menu */}
+        <div className="hidden md:flex items-center justify-center gap-7 mr-3">
+          <SearchBar />
+          <SettingsIcon />
+          <ProfileIcon />
+        </div>
+
+        {/* Mobile Hamburger */}
+        <button
+          className="block md:hidden text-3xl"
+          onClick={() => setOpenMenu(!openMenu)}
+        >
+          {openMenu ? <IoClose /> : <GiHamburgerMenu />}
+        </button>
       </div>
-    </div>
+
+      {/* MOBILE MENU DROPDOWN */}
+      {openMenu && (
+        <div className="md:hidden w-full p-4 flex items-center justify-center  flex-wrap gap-5 bg-[#161b22] text-white border-b border-gray-600">
+          <SearchBar />
+          <SettingsIcon />
+          <ProfileIcon />
+        </div>
+      )}
+    </>
   );
 };
 
--- git-scope/app/components/ui/icons/ProfileIcon.tsx
+++ git-scope/app/components/ui/icons/ProfileIcon.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+"use client";
+import React from "react";
+import { useStore } from "@/app/lib/store";
 
 const ProfileIcon = () => {
+  const username = useStore((state) => state.username);
+  const theme = useStore((state) => state.theme);
+
+  // First letter → uppercase
+  const firstLetter = username ? username.charAt(0).toUpperCase() : "?";
+
   return (
-    <div className="h-10 w-10 rounded-full bg-linear-to-br from-gray-300 to-gray-500 flex items-center justify-center text-white font-semibold shadow-md">
-      <span>G</span>
+    <div
+      className={`
+        h-10 w-10 rounded-full flex items-center justify-center font-semibold shadow-md
+        transition-all duration-300
+        ${
+          theme === "Dark"
+            ? "bg-linear-to-br from-[#3b82f6] to-[#1f2937] text-white"
+            : "bg-linear-to-br from-blue-400 to-gray-300 text-black"
+        }
+      `}
+    >
+      <span>{firstLetter}</span>
     </div>
   );
 };
